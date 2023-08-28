2d1a92c49f32e4b753bcf31eb7df16996be28dac
Alex <98567345+Axelito02@users.noreply.github.com>
2023-08-28 18:12:57 -0500

    subiendo solo el contador, sin las carpetas adicionales

--- clase1- componentes/main.js
+++
@@ -1,4 +0,0 @@
-import './style.css'
-import './components/HeaderComponent'
-import './components/buttons'
-import './components/counter'
--- reactjs/reac(1)/src/components/Content.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-export function Content(props) {
-    return (
-        <main>
-                <button>Presioname!!!</button>
-        </main>
-    )
-}
